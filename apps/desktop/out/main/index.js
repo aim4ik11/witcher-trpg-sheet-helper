b65d875c2a361714ea2a4eef372a026ddea65fc1
Patriot0911 <alexandr.potaskalov@gmail.com>
2026-06-22 14:22:55 +0300

    feat: add dev tools to desktop dev app

--- apps/desktop/out/main/index.js
+++ apps/desktop/out/main/index.js
@@ -24,8 +24,12 @@ function createWindow() {
       sandbox: false
     }
   });
-  if (isDev) void win.loadURL(process.env.ELECTRON_RENDERER_URL);
-  else void win.loadFile(path.join(__dirname, "../renderer/index.html"));
+  if (isDev) {
+    void win.loadURL(process.env.ELECTRON_RENDERER_URL);
+    win.webContents.openDevTools();
+  } else {
+    void win.loadFile(path.join(__dirname, "../renderer/index.html"));
+  }
 }
 function startServer(config) {
   return new Promise((resolve, reject) => {
